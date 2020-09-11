c7d215b13596f6062a89e59d363b9d6cd0b89bd0
Zhen Ju <juzhen@huawei.com>
2020-09-11 18:40:48 +0800

    Fixes & improments following the code review comments.
    * Update the meta info in package.xml and setup.py
    * Try-except executor.spin instead of main in executables
    * Use f string to format infos
    * Pass error message to response objectc instead of logging it
    * Use node's logger instead of init a global one
    * Call node.get_qualified_name() to assign response obj's full_node_name
    * Other super tiny changes
    
    Signed-off-by: Zhen Ju <juzhen@huawei.com>

--- rclpy_components/setup.py
+++ rclpy_components/setup.py
@@ -4,7 +4,7 @@ package_name = 'rclpy_components'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.1.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,10 +13,10 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='root@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Zhen Ju',
+    maintainer_email='juzhen@huawei.com',
+    description='The dynamic node management package',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
